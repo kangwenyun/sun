109c13864a1f133b533e8446b9ceb9128ddfb4d1
Cassie <798218157@qq.com>
2016-06-10 19:05:59 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/sun/app/sun/model/Province.java
@@ -0,0 +1,34 @@
+package com.sun.app.sun.model;
+
+/**
+ * Created by Administrator on 2016/6/10.
+ */
+public class Province {
+    private int id;
+    private String provinceName ;
+    private String provinceCode ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
