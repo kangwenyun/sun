788a6c3b34cac34d9fd5d92720a2ee9c83cc5469
Cassie <798218157@qq.com>
2016-06-14 16:16:42 +0800

    还差后台自动更新

---
+++ app/src/main/java/com/sun/app/sun/service/AutoUpdateService.java
@@ -0,0 +1,27 @@
+//package com.sun.app.sun.service;
+//
+//import android.app.AlarmManager;
+//import android.app.Service;
+//import android.content.Intent;
+//import android.os.IBinder;
+//import android.support.annotation.Nullable;
+//
+///**
+// * Created by Administrator on 2016/6/14.
+// */
+//public class AutoUpdateService extends Service {
+//    @Nullable
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null;
+//    }
+//    public int onStartCommand(Intent intent,int flags,int startId){
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }).start();
+//        AlarmManager manager =
+//    }
+//}
