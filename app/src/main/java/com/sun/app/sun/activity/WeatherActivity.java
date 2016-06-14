6d48e80886f181056b9db2b529704a2d27640cdc
Cassie <798218157@qq.com>
2016-06-14 15:11:57 +0800

    天气描述有问题

--- app/src/main/java/com/sun/app/sun/activity/WeatherActivity.java
+++ app/src/main/java/com/sun/app/sun/activity/WeatherActivity.java
@@ -106,9 +106,9 @@ public class WeatherActivity extends Activity {
         cityNameText.setText(prefs.getString("city_name",""));
         temp1Text.setText(prefs.getString("temp1",""));
         temp2Text.setText(prefs.getString("temp2",""));
-        weatherDespText.setText(prefs.getString("weather_desp",""));
+        weatherDespText.setText(prefs.getString("weather_desp","无"));
         publishText.setText("今天" + prefs.getString("publish_time","") + "发布");
-        currentDateText.setText(prefs.getString("current_data",""));
+        currentDateText.setText(prefs.getString("current_date",""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
     }
--- app/src/main/java/com/sun/app/sun/util/Utility.java
+++ app/src/main/java/com/sun/app/sun/util/Utility.java
@@ -103,7 +103,7 @@ public class Utility {
         editor.putString("weather_code", weatherCode);
         editor.putString("temp1", temp1);
         editor.putString("temp2", temp2);
-        editor.putString("weather_disp", weatherDesp);
+        editor.putString("weather_desp", weatherDesp);
         editor.putString("publish_time", publishTime);
         editor.putString("current_date",sdf.format(new Date()));
         editor.commit();
