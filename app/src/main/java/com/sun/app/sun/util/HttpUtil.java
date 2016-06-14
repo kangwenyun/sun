3d4c38a8ef3a4631ff2d3861a8d8940eb389edca
Cassie <798218157@qq.com>
2016-06-14 14:54:54 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sun/app/sun/util/HttpUtil.java
+++ app/src/main/java/com/sun/app/sun/util/HttpUtil.java
@@ -1,11 +1,11 @@
 package com.sun.app.sun.util;
 
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.Buffer;
 
 /**
  * Created by Administrator on 2016/6/10.
